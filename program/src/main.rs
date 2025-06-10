37ee906efc9ad8ecbf742ef0b04980e7ebe66c49
Rahul Ghangas <rahulghangas0@gmail.com>
2025-06-10 21:41:06 +0530

    feat: update program to verify proof

--- program/src/main.rs
+++ program/src/main.rs
@@ -11,29 +11,59 @@ use num_traits::Zero;
 use stwo_verifier_no_std::{
     air::Component,
     channel::Blake2sChannel,
-    constraint_framework::{FrameworkComponent, TraceLocationAllocator},
-    examples::WideFibonacciEval,
-    fields::qm31::SecureField,
+    constraint_framework::{EvalAtRow, FrameworkComponent, FrameworkEval, TraceLocationAllocator},
+    fields::{qm31::SecureField, FieldExpOps},
     pcs::{CommitmentSchemeVerifier, PcsConfig},
     vcs::blake2_merkle::{Blake2sMerkleChannel, Blake2sMerkleHasher},
     verify, StarkProof,
 };
 sp1_zkvm::entrypoint!(main);
 
+#[derive(Clone)]
+pub struct WideFibonacciEval<const N: usize> {
+    pub log_n_rows: u32,
+}
+
+impl<const N: usize> FrameworkEval for WideFibonacciEval<N> {
+    fn log_size(&self) -> u32 {
+        self.log_n_rows
+    }
+    fn max_constraint_log_degree_bound(&self) -> u32 {
+        self.log_n_rows + 1
+    }
+    fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
+        let mut a = eval.next_trace_mask();
+        let mut b = eval.next_trace_mask();
+        for _ in 2..N {
+            let c = eval.next_trace_mask();
+            eval.add_constraint(c.clone() - (a.square() + b.square()));
+            a = b;
+            b = c;
+        }
+        eval
+    }
+}
+
 pub fn main() {
-    let proof_serialized = sp1_zkvm::io::read_vec();
-    let proof: StarkProof<Blake2sMerkleHasher> = serde_json::from_slice(&proof_serialized).unwrap();
-    let eval_serialized = sp1_zkvm::io::read_vec();
-    let eval: WideFibonacciEval<100> = serde_json::from_slice(&eval_serialized).unwrap();
+    let log_n_instances: u32 = sp1_zkvm::io::read();
+    let proof: StarkProof<Blake2sMerkleHasher> = sp1_zkvm::io::read();
+
     let config = PcsConfig::default();
     let verifier_channel = &mut Blake2sChannel::default();
     let commitment_scheme = &mut CommitmentSchemeVerifier::<Blake2sMerkleChannel>::new(config);
     let mut location_allocator = TraceLocationAllocator::default();
-    let component = FrameworkComponent::new(&mut location_allocator, eval, SecureField::zero());
+    let component = FrameworkComponent::new(
+        &mut location_allocator,
+        WideFibonacciEval::<100> {
+            log_n_rows: log_n_instances,
+        },
+        SecureField::zero(),
+    );
     let sizes = component.trace_log_degree_bounds();
 
     commitment_scheme.commit(proof.commitments[0], &sizes[0], verifier_channel);
     commitment_scheme.commit(proof.commitments[1], &sizes[1], verifier_channel);
+
     let result = verify(&[&component], verifier_channel, commitment_scheme, proof);
     sp1_zkvm::io::commit(if result.is_ok() { &1_u8 } else { &0 });
 }
