8bbd5c9eea07327d2e0bb390934dc446d489124a
Suraj Choudhary <suraj@aeriuslabs.org>
2025-06-26 12:19:55 +0530

    chore: update deps

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -11,39 +11,21 @@
 //! ```
 
 use clap::Parser;
-use itertools::Itertools;
-use num_traits::{One, Zero};
+use rand::Rng;
 use sp1_sdk::{ProverClient, SP1Stdin};
-
-use stwo_prover::constraint_framework::TraceLocationAllocator;
-use stwo_prover::constraint_framework::{
-    AssertEvaluator, EvalAtRow, FrameworkComponent, FrameworkEval,
-};
-use stwo_prover::core::backend::simd::m31::PackedBaseField;
-use stwo_prover::core::backend::simd::m31::LOG_N_LANES;
-use stwo_prover::core::backend::simd::SimdBackend;
-use stwo_prover::core::backend::{Col, Column};
-use stwo_prover::core::channel::Blake2sChannel;
-use stwo_prover::core::fields::m31::BaseField;
-use stwo_prover::core::fields::qm31::SecureField;
-use stwo_prover::core::fields::FieldExpOps;
-use stwo_prover::core::pcs::{CommitmentSchemeProver, PcsConfig};
-use stwo_prover::core::poly::circle::PolyOps;
-use stwo_prover::core::poly::circle::{CanonicCoset, CircleEvaluation};
-use stwo_prover::core::poly::BitReversedOrder;
-use stwo_prover::core::prover::{prove, StarkProof};
-use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleChannel;
+use std::{cell::RefCell, rc::Rc};
 use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher;
-use stwo_prover::core::ColumnVec;
-
-pub type WideFibonacciComponent<const N: usize> = FrameworkComponent<WideFibonacciEval<N>>;
+use vex_prover::{
+    executor::{order_book::OrderBook, record::ExecutionTrace},
+    imt::order::Order,
+    prover::prove_vex,
+    VexProof,
+};
 
 /// Read ELF file for the program.
 pub const FIBONACCI_ELF: &[u8] = include_bytes!(
     "../../../program/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/fibonacci-program"
 );
-
-const LOG_N_INSTANCES: u32 = 6;
 
 /// The arguments for the command.
 #[derive(Parser, Debug)]
@@ -56,137 +38,37 @@ struct Args {
     prove: bool,
 }
 
-pub struct FibInput {
-    a: PackedBaseField,
-    b: PackedBaseField,
-}
-
-/// A component that enforces the Fibonacci sequence.
-/// Each row contains a seperate Fibonacci sequence of length `N`.
-#[derive(Clone)]
-pub struct WideFibonacciEval<const N: usize> {
-    pub log_n_rows: u32,
-}
-
-impl<const N: usize> FrameworkEval for WideFibonacciEval<N> {
-    fn log_size(&self) -> u32 {
-        self.log_n_rows
+fn generate_proof() -> VexProof<Blake2sMerkleHasher> {
+    let record = Rc::new(RefCell::new(ExecutionTrace::new()));
+    let mut order_book = OrderBook::new(Rc::clone(&record));
+    let mut rng = rand::thread_rng();
+    let mut time = 1;
+    let n = 1 << 7;
+    for _ in 0..n {
+        let time_inc = rng.gen_range(1..=16);
+        time += time_inc;
+        let buy_order = Order::new(
+            rng.gen_range(100000..10000000),
+            rng.gen_range(1000000..=1000990),
+            time,
+        );
+        let sell_order = Order::new(
+            rng.gen_range(100000..10000000),
+            rng.gen_range(1000000..=1000990),
+            time,
+        );
+        order_book.place_buy_order(buy_order).unwrap();
+        order_book.place_sell_order(sell_order).unwrap();
     }
-    fn max_constraint_log_degree_bound(&self) -> u32 {
-        self.log_n_rows + 1
-    }
-    fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
-        let mut a = eval.next_trace_mask();
-        let mut b = eval.next_trace_mask();
-        for _ in 2..N {
-            let c = eval.next_trace_mask();
-            eval.add_constraint(c.clone() - (a.square() + b.square()));
-            a = b;
-            b = c;
-        }
-        eval
-    }
-}
 
-pub fn generate_trace<const N: usize>(
-    log_size: u32,
-    inputs: &[FibInput],
-) -> ColumnVec<CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>> {
-    let mut trace = (0..N)
-        .map(|_| Col::<SimdBackend, BaseField>::zeros(1 << log_size))
-        .collect_vec();
-    for (vec_index, input) in inputs.iter().enumerate() {
-        let mut a = input.a;
-        let mut b = input.b;
-        trace[0].data[vec_index] = a;
-        trace[1].data[vec_index] = b;
-        trace.iter_mut().skip(2).for_each(|col| {
-            (a, b) = (b, a.square() + b.square());
-            col.data[vec_index] = b;
-        });
-    }
-    let domain = CanonicCoset::new(log_size).circle_domain();
-    trace
-        .into_iter()
-        .map(|eval| CircleEvaluation::<SimdBackend, _, BitReversedOrder>::new(domain, eval))
-        .collect_vec()
-}
-
-fn generate_test_trace(
-    log_n_instances: u32,
-) -> ColumnVec<CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>> {
-    if log_n_instances < LOG_N_LANES {
-        let n_instances = 1 << log_n_instances;
-        let inputs = vec![FibInput {
-            a: PackedBaseField::from_array(std::array::from_fn(|j| {
-                if j < n_instances {
-                    BaseField::one()
-                } else {
-                    BaseField::zero()
-                }
-            })),
-            b: PackedBaseField::from_array(std::array::from_fn(|j| {
-                if j < n_instances {
-                    BaseField::from_u32_unchecked((j) as u32)
-                } else {
-                    BaseField::zero()
-                }
-            })),
-        }];
-        return generate_trace::<100>(log_n_instances, &inputs);
-    }
-    let inputs = (0..(1 << (log_n_instances - LOG_N_LANES)))
-        .map(|i| FibInput {
-            a: PackedBaseField::one(),
-            b: PackedBaseField::from_array(std::array::from_fn(|j| {
-                BaseField::from_u32_unchecked((i * 16 + j) as u32)
-            })),
-        })
-        .collect_vec();
-    generate_trace::<100>(log_n_instances, &inputs)
-}
-
-fn generate_proof(log_n_instances: u32) -> StarkProof<Blake2sMerkleHasher> {
-    let config = PcsConfig::default();
-    // Precompute twiddles.
-    let twiddles = SimdBackend::precompute_twiddles(
-        CanonicCoset::new(log_n_instances + 1 + config.fri_config.log_blowup_factor)
-            .circle_domain()
-            .half_coset,
-    );
-
-    // Setup protocol.
-    let prover_channel = &mut Blake2sChannel::default();
-    let mut commitment_scheme =
-        CommitmentSchemeProver::<SimdBackend, Blake2sMerkleChannel>::new(config, &twiddles);
-
-    // Preprocessed trace
-    let mut tree_builder = commitment_scheme.tree_builder();
-    tree_builder.extend_evals([]);
-    tree_builder.commit(prover_channel);
-
-    // Trace.
-    let trace = generate_test_trace(log_n_instances);
-    let mut tree_builder = commitment_scheme.tree_builder();
-    tree_builder.extend_evals(trace);
-    tree_builder.commit(prover_channel);
-
-    // Prove constraints.
-    let component = WideFibonacciComponent::new(
-        &mut TraceLocationAllocator::default(),
-        WideFibonacciEval::<100> {
-            log_n_rows: log_n_instances,
-        },
-        SecureField::zero(),
-    );
-
-    let proof = prove::<SimdBackend, Blake2sMerkleChannel>(
-        &[&component],
-        prover_channel,
-        commitment_scheme,
-    )
-    .unwrap();
-
+    let mut execution_trace = std::mem::replace(&mut *record.borrow_mut(), ExecutionTrace::new());
+    execution_trace.final_state = order_book.state().to_felts();
+    let shape = execution_trace.sizes();
+    println!("Execution trace shape: {:?}", shape);
+    let start = std::time::Instant::now();
+    let proof = prove_vex(execution_trace).unwrap();
+    let end = start.elapsed();
+    println!("Proof generated in {} ms", end.as_millis());
     proof
 }
 
@@ -203,14 +85,13 @@ fn main() {
         std::process::exit(1);
     }
 
-    let proof = generate_proof(LOG_N_INSTANCES);
+    let proof = generate_proof();
 
     // Setup the prover client.
     let client = ProverClient::from_env();
 
     // Setup the inputs.
     let mut stdin = SP1Stdin::new();
-    stdin.write(&LOG_N_INSTANCES);
     stdin.write(&proof);
 
     if args.execute {
